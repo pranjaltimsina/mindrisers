98da027b0cd7c1bc58d4573525ae6487811f84c5
Pranjal Timsina <pranjal.timsina@live.com>
2022-01-26 16:20:51 +0545

    fix: splash screen padding on small screens

--- src/components/HomePageComponents/Splash.tsx
+++ src/components/HomePageComponents/Splash.tsx
@@ -1,7 +1,7 @@
 const Splash = () => {
   return (
     <div className='bg-gray-800 flex flex-col items-center w-full h-screen'>
-      <div className='mt-52 text-md text-green-400'>MANAGED IT SOLUTIONS</div>
+      <div className='mt-28 sm:mt-52 text-md text-green-400'>MANAGED IT SOLUTIONS</div>
       <h1 className='h-max mt-2 w-4/5 sm:w-1/2 text-center text-white font-sans font-extrabold text-6xl'>
         Reliable, scalable and built for your business.
       </h1>
