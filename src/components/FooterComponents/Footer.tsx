5c1dc33689f43943c266a034cc2dc227475989b8
Pranjal Timsina <pranjal.timsina@live.com>
2022-02-04 17:35:46 +0545

    fix: content overflow on footer in small devices due to too much padding

--- src/components/FooterComponents/Footer.tsx
+++ src/components/FooterComponents/Footer.tsx
@@ -6,7 +6,7 @@ import FooterPageLinks from './FooterPageLinks'
 const Footer = () => {
   return (
     <footer className='text-white w-full h-max sm:h-max '>
-      <div className='flex h-max w-full flex-col sm:flex-row sm:justify-between bg-slate-800 px-56 pt-20 pb-10 overflow-x-hidden'>
+      <div className='flex h-max w-full flex-col sm:flex-row sm:justify-between bg-slate-800 px-10 sm:px-56 pt-20 pb-10 overflow-x-hidden'>
         <CompanyInfo />
         <FooterPageLinks />
         <FooterContact />
