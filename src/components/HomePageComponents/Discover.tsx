e7dc4c1755716fd3000d9243fcc239223f36de22
Pranjal Timsina <pranjal.timsina@live.com>
2022-02-06 16:23:33 +0545

    chore: update content in the discover cards

--- src/components/HomePageComponents/Discover.tsx
+++ src/components/HomePageComponents/Discover.tsx
@@ -4,26 +4,49 @@ import Placeholder from '../../assets/discover-card-img.jpg'
 const Discover = () => {
   return (
     <section className='bg-neutral-100 flex flex-col py-20 sm:py-24 px-5 sm:px-14 md:px-18 lg:px-32 xl:px-44'>
-      <h1 className='w-full text-center text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold font-sans'>Discover the tech innovations</h1>
+      <h1 className='w-full text-center text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold font-sans'>Services we provide</h1>
       <div className='flex flex-col  md:flex-row flex-wrap w-full justify-between'>
-        <DiscoverCard title='This' info='this' image={
-          <img src={Placeholder} alt='This'/>
-        }/>
-        <DiscoverCard title='This' info='this' image={
-          <img src={Placeholder} alt='This'/>
-        }/>
-        <DiscoverCard title='This' info='this' image={
-          <img src={Placeholder} alt='This'/>
-        }/>
-        <DiscoverCard title='This' info='this' image={
-          <img src={Placeholder} alt='This'/>
-        }/>
-        <DiscoverCard title='This' info='this' image={
-          <img src={Placeholder} alt='This'/>
-        }/>
-        <DiscoverCard title='This' info='this' image={
-          <img src={Placeholder} alt='This'/>
-        }/>
+        <DiscoverCard
+          title='Database Migrations'
+          info='We facilitate reliable, hassle-free database migrations.'
+          image={
+            <img src={Placeholder} alt='Database Migrations'/>
+          }
+        />
+        <DiscoverCard
+          title='Managed Hostings'
+          info="We manage and host what you want us to. You won't have to worry about hostings ever again."
+          image={
+            <img src={Placeholder} alt='Managed Hostings'/>
+          }
+        />
+        <DiscoverCard
+          title='Web Development' info='We make the best in class modern web applications. Contact us for a quote.'
+          image={
+            <img src={Placeholder} alt='Web Development'/>
+          }
+        />
+        <DiscoverCard
+          title='Mobile Apps'
+          info='We make top-notch mobile applications for both Android and IOS.'
+          image={
+            <img src={Placeholder} alt='Mobile Apps'/>
+          }
+        />
+        <DiscoverCard
+          title='Enterprise Softwares'
+          info='The enterprise level applications we make are robust, secure and reliable.'
+          image={
+            <img src={Placeholder} alt='Enterprise Softwares'/>
+          }
+        />
+        <DiscoverCard
+          title='Digital Marketing'
+          info='We provide all kinds of digital marketing services.'
+          image={
+            <img src={Placeholder} alt='Digital Marketing'/>
+          }
+        />
       </div>
     </section>
   )
--- src/components/HomePageComponents/DiscoverCard.tsx
+++ src/components/HomePageComponents/DiscoverCard.tsx
@@ -6,12 +6,14 @@ interface DiscoverCardInterface {
 
 const DiscoverCard = ({ image, title, info }: DiscoverCardInterface) => {
   return (
-    <div className='bg-white md:w-[45%] lg:w-[30%] mt-12 flex flex-col rounded-md hover:rounded-none'>
-      <div className='image-box rounded-md hover:rounded-none duration-500'>
+    <div className='image-box bg-white md:w-[45%] lg:w-[30%] mt-12 flex flex-col rounded-md hover:rounded-none duration-500'>
+      <div className='image-box'>
         {image}
       </div>
       <h4 className='font-sans text-neutral-600 font-bold text-xl p-5'>{title}</h4>
-      <p className="font-sans text-neutral-700 text-base p-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit, unde.</p>
+      <p className="font-sans text-neutral-700 text-base p-5">
+        {info}
+      </p>
       <p className="font-sans text-green-500 text-base p-5 underline underline-offset-4 font-normal cursor-pointer w-max decoration-current duration-500 hover:decoration-white" >Learn More</p>
     </div>
   )
