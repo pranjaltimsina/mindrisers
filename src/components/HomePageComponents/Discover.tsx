22343a658be8a204045bc53c3e0a704892f0a696
Pranjal Timsina <pranjal.timsina@live.com>
2022-03-01 10:21:34 +0545

    chore: update tech services title

--- src/components/HomePageComponents/Discover.tsx
+++ src/components/HomePageComponents/Discover.tsx
@@ -10,7 +10,7 @@ import Marketing from '../../assets/discover/marketing.jpg'
 const Discover = () => {
   return (
     <section className='bg-neutral-100 flex flex-col py-20 sm:py-24 px-5 sm:px-14 md:px-18 lg:px-32 xl:px-44'>
-      <h1 className='w-full text-center text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold font-sans'>Services we provide</h1>
+      <h1 className='w-full text-center text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold font-sans'>Tech services we provide</h1>
       <div className='flex flex-col  md:flex-row flex-wrap w-full justify-between'>
         <DiscoverCard
           title='Database Migrations'
