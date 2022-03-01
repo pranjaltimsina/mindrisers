f063033f98d273e072d2f1bc925e215e8c9f470a
Pranjal Timsina <pranjal.timsina@live.com>
2022-03-01 13:05:24 +0545

    fix: dropdown size on small screens

--- src/components/HeaderComponents/Dropdown.tsx
+++ src/components/HeaderComponents/Dropdown.tsx
@@ -2,7 +2,7 @@ import HeaderTabs from './HeaderTabs'
 
 const Dropdown = () => {
   return (
-    <div className='w-full h-1/3 bg-white fixed top-[77px] m-0 right-0'>
+    <div className='w-full h-2/3 bg-white fixed z-[10] top-[77px] m-0 right-0'>
       <HeaderTabs />
     </div>
   )
